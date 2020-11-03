55d5a0e6472167e07f12538a6769ef5c8d7c4dfa
Jorge <jorge.lopez@live.u-tad.com>
2020-11-03 16:41:46 +0100

    Descarga de apks e intento de instalarlas

--- app/src/main/java/com/example/apkdownloader/MainActivity.java
+++ app/src/main/java/com/example/apkdownloader/MainActivity.java
@@ -1,14 +1,59 @@
-package com.example.apkdownloader;
+    package com.example.apkdownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    //http://jlfpg.github.io/images/APKPure_v3.17.14_apkpure.com.apk
+    Button btnDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+
+        btnDownload = (Button) findViewById(R.id.btnDownload);
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*try {
+                    ProgressDialog progress;
+                    InstallAPK downloadAndInstall = new InstallAPK();
+                    progress = new ProgressDialog(MainActivity.this);
+                    progress.setCancelable(false);
+                    progress.setMessage("Descargando la app...");
+                    downloadAndInstall.setContext(getApplicationContext());
+                    downloadAndInstall.execute("http://jlfpg.github.io/images/app.apk");
+                } catch (Exception e) {
+
+                }*/
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) ,"/prueba.apk")), "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }

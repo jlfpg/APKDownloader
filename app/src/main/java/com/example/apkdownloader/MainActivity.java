eea7b917c5d1fffeffbe32b6d9aaf56a127f2145
Jorge <jorge.lopez@live.u-tad.com>
2020-11-29 19:41:54 +0100

    Descarga e instalación del apk
    Descarga e instalación del apk  funcional al 100%

--- app/src/main/java/com/example/apkdownloader/MainActivity.java
+++ app/src/main/java/com/example/apkdownloader/MainActivity.java
@@ -1,69 +1,134 @@
-    package com.example.apkdownloader;
+package com.example.apkdownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
+import androidx.core.content.FileProvider;
 import android.Manifest;
-import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
-
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.DownloadListener;
+import com.androidnetworking.interfaces.DownloadProgressListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    //http://jlfpg.github.io/images/APKPure_v3.17.14_apkpure.com.apk
+    //Variables goblales
+    private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 0;
+    public static final int progress_bar_type = 0;
+    private static String file_url = "http://jlfpg.github.io/images/app.apk";
     Button btnDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Pantalla completa
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        //Comprobar los permisos
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        AndroidNetworking.initialize(getApplicationContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (!getPackageManager().canRequestPackageInstalls()) {
+                startActivityForResult(new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES).setData(Uri.parse(String.format("package:%s", getPackageName()))), 1234);
+            } else {
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        
         btnDownload = (Button) findViewById(R.id.btnDownload);
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*try {
-                    ProgressDialog progress;
-                    InstallAPK downloadAndInstall = new InstallAPK();
-                    progress = new ProgressDialog(MainActivity.this);
-                    progress.setCancelable(false);
-                    progress.setMessage("Descargando la app...");
-                    downloadAndInstall.setContext(getApplicationContext());
-                    downloadAndInstall.execute("http://jlfpg.github.io/images/app.apk");
-                } catch (Exception e) {
+                String fileName ="app.apk";
+                String dirPath=Environment.getExternalStorageDirectory().toString() + "/Download/APKDownloader/";
+                AndroidNetworking.download(file_url,dirPath,fileName)
+                        .setTag("downloadTest")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .setDownloadProgressListener(new DownloadProgressListener() {
+                            @Override
+                            public void onProgress(long bytesDownloaded, long totalBytes) {
+                                // do anything with progress
+                            }
+                        })
+                        .startDownload(new DownloadListener() {
+                            @Override
+                            public void onDownloadComplete() {
+                                // do anything after completion
+                            }
+                            @Override
+                            public void onError(ANError error) {
+                                // handle error
+                            }
+                        });
 
-                }*/
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) ,"/prueba.apk")), "application/vnd.android.package-archive");
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                String PATH = Environment.getExternalStorageDirectory() + "/Download/APKDownloader/" + "app.apk";
+                File file = new File(PATH);
+                if(file.exists()) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(uriFromFile(getApplicationContext(), new File(PATH)), "application/vnd.android.package-archive");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    try {
+                        getApplicationContext().startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                        Log.e("TAG", "Error in opening the file!");
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"installing",Toast.LENGTH_LONG).show();
+                }
+            }
+            Uri uriFromFile(Context context, File file) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    return FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", file);
+                } else {
+                    return Uri.fromFile(file);
+                }
             }
         });
     }

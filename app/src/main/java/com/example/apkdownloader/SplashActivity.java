1b3b6e914eb146d11e15bd2f19c4766f9daa3d84
Jorge <jorge.lopez@live.u-tad.com>
2020-11-29 20:49:22 +0100

    Diseño

--- app/src/main/java/com/example/apkdownloader/SplashActivity.java
+++ app/src/main/java/com/example/apkdownloader/SplashActivity.java
@@ -1,14 +1,29 @@
 package com.example.apkdownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static int TIME_OUT = 700;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
